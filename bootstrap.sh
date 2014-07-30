fac45dfd435201d271e8323a3253fb5a282a5483
Jonathan Munz <jp_137@hotmail.com>
2014-07-30 10:04:44 -0400

    get packages working

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,39 @@
-#!/usr/env bash
+#!/usr/bin/env bash
 
-command -v ansible >/dev/null 2>&1
+function available {
+    command -v $1 >/dev/null 2>&1
+}
 
-if [ $? -ne 0 ]; then
-    git clone git://github.com/ansible/ansible.git
-    cd ./ansible
-    source ./hacking/env-setup
+function install_pkg {
+    if $(available apt-get) ; then
+        PKG_MANAGER="apt-get"
+    else
+        PKG_MANAGER="yum"
+    fi
+
+    sudo $PKG_MANAGER -y install $1
+
+    if [ $? -ne 0 ] ; then
+        exit 1
+    fi 
+}
+
+function install_ansible {
+    if ! $(available pip) ; then
+        install_pkg python-dev
+        install_pkg python-pip
+    fi
+
+    sudo pip install ansible
+
+    if [ $? -ne 0 ] ; then
+        exit 1
+    fi 
+}
+
+if ! $(available ansible) ; then
+    echo "Ansible not available installing..."
+    install_ansible >/dev/null
 fi
 
-ansible-playbook site.yml
+ansible-playbook -i localhost, main.yml
