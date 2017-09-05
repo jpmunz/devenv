20a663a27db2fea6859c9e2dcbef7d8a10b47622
Jon <jpmunz@gmail.com>
2017-09-04 21:41:27 -0400

    centos7 disable firewall

--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -1,18 +1,6 @@
-# Allow everything through the firewall
-iptables -F
-iptables -X
-iptables -t nat -F
-iptables -t nat -X
-iptables -t mangle -F
-iptables -t mangle -X
-iptables -P INPUT ACCEPT
-iptables -P FORWARD ACCEPT
-iptables -P OUTPUT ACCEPT
-
-# Persist the firewall rules
-yum install -y iptables-services
-systemctl enable iptables.service
-/usr/libexec/iptables/iptables.init save
+# Disable the firewall
+systemctl disable firewalld
+systemctl stop firewalld
 
 # Setup dev environment
 cd /vagrant/
