9398d1623cac63e6676e4ed8a1db6b429a345bd4
Jonathan Munz <jpmunz@gmail.com>
2021-03-13 18:04:37 -0500

    Mac setup

---
+++ mac-setup.sh
@@ -0,0 +1,22 @@
+if ! command -v brew
+then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+xcode-select --install
+
+brew install firefox
+brew install google-chrome
+brew install slack
+brew install zoom
+brew install iterm2
+brew install tmux
+brew install pycharm
+brew install dropbox
+brew install rectangle
+brew install ack
+brew install diff-so-fancy
+brew install tree
+brew install wget
+
+defaults write NSGlobalDomain KeyRepeat -int 3
